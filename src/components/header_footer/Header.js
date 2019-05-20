32c71b4af0b7762b4ac413f4e8a1af5e1a95472c
user <hunchan12@gmail.com>
2019-05-20 20:52:28 +0900

    Header Sidedrawer

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -4,25 +4,53 @@ import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/core/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
+import SideDrawer from './SideDrawer';
 
 class Header extends Component {
+
+    state = {
+        drawerOpen: false
+    }
+
+    toggleDrawer = (value) => {
+        this.setState({
+            drawerOpen: value
+        })
+    }
+
+
     render() {
         return (
-            <AppBar
-                position="fixed"
-                style={{
-                    backgroundColor: '#2f2f2f',
-                    boxShadow: 'none',
-                    padding: '10px 0px'
-                }}
-            >
-                <Toolbar>
-                    <div className="header_logo">
-                        <div className="font_roboto header_logo_venue">The Venue</div>
-                        <div className="header_logo_title">Musical</div>
-                    </div>
-                </Toolbar>
-            </AppBar>
+            <div>
+                <AppBar
+                    position="fixed"
+                    style={{
+                        backgroundColor: '#2f2f2f',
+                        boxShadow: 'none',
+                        padding: '10px 0px'
+                    }}
+                >
+                    <Toolbar>
+                        <div className="header_logo">
+                            <div className="font_roboto header_logo_venue">The Venue</div>
+                            <div className="header_logo_title">Musical</div>
+                        </div>
+
+                        <IconButton
+                            aria-label="Menu"
+                            color="inherit"
+                            onClick={() => this.toggleDrawer(true)}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+
+                        <SideDrawer
+                            open={this.state.drawerOpen}
+                            onClose={(value) => this.toggleDrawer(value)}
+                        />
+                    </Toolbar>
+                </AppBar>
+            </div>
         )
     }
 }
