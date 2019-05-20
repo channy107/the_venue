80d17474ced28084055552a4505a65e64ca7941e
user <hunchan12@gmail.com>
2019-05-20 21:50:51 +0900

    Slider section

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import MenuIcon from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
 import SideDrawer from './SideDrawer';
@@ -9,7 +9,24 @@ import SideDrawer from './SideDrawer';
 class Header extends Component {
 
     state = {
-        drawerOpen: false
+        drawerOpen: false,
+        headerShow: false
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+        if (window.scrollY > 0) {
+            this.setState({
+                headerShow: true
+            })
+        } else {
+            this.setState({
+                headerShow: false
+            })
+        }
     }
 
     toggleDrawer = (value) => {
@@ -21,36 +38,34 @@ class Header extends Component {
 
     render() {
         return (
-            <div>
-                <AppBar
-                    position="fixed"
-                    style={{
-                        backgroundColor: '#2f2f2f',
-                        boxShadow: 'none',
-                        padding: '10px 0px'
-                    }}
-                >
-                    <Toolbar>
-                        <div className="header_logo">
-                            <div className="font_roboto header_logo_venue">The Venue</div>
-                            <div className="header_logo_title">Musical</div>
-                        </div>
+            <AppBar
+                position="fixed"
+                style={{
+                    backgroundColor: this.state.headerShow ? '#2f2f2f' : 'transparent',
+                    boxShadow: 'none',
+                    padding: '10px 0px'
+                }}
+            >
+                <Toolbar>
+                    <div className="header_logo">
+                        <div className="font_roboto header_logo_venue">The Venue</div>
+                        <div className="header_logo_title">Musical</div>
+                    </div>
 
-                        <IconButton
-                            aria-label="Menu"
-                            color="inherit"
-                            onClick={() => this.toggleDrawer(true)}
-                        >
-                            <MenuIcon />
-                        </IconButton>
+                    <IconButton
+                        aria-label="Menu"
+                        color="inherit"
+                        onClick={() => this.toggleDrawer(true)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
 
-                        <SideDrawer
-                            open={this.state.drawerOpen}
-                            onClose={(value) => this.toggleDrawer(value)}
-                        />
-                    </Toolbar>
-                </AppBar>
-            </div>
+                    <SideDrawer
+                        open={this.state.drawerOpen}
+                        onClose={(value) => this.toggleDrawer(value)}
+                    />
+                </Toolbar>
+            </AppBar>
         )
     }
 }
