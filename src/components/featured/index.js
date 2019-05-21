d28b2bfcea46b424df752cd8c2316dbb653fae0e
user <hunchan12@gmail.com>
2019-05-21 10:23:10 +0900

    Adding countdown

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carrousel from './Carrousel';
+import TimeUntil from './Timeuntil';
 
 const Featured = () => {
     return (
@@ -10,6 +11,8 @@ const Featured = () => {
                     Ariana Grande
                 </div>
             </div>
+
+            <TimeUntil />
         </div>
     );
 };
