a59881787331aeb288b7d89289b5a128a98ce6c6
user <hunchan12@gmail.com>
2019-05-21 15:10:50 +0900

    Venue NFO component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
+import VenuedNfo from './components/venueNfo';
 
 function App() {
   return (
     <div className="App" style={{ height: "1500px", background: 'cornflowerblue' }}>
       <Header />
       <Featured />
+      <VenuedNfo />
     </div>
   );
 }
